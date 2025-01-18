7f850e5936b6dc1f18ff56fd87c597f97ab6e11b
William <william.crepault@gmail.com>
2025-01-17 22:40:13 -0500

    Enlevé comment en bas...

--- src/Program.cpp
+++ src/Program.cpp
@@ -32,21 +32,3 @@ void Program::loop() {
         printValues();
     }
 }
-
-
-
-
-
-
-// private:
-//     ADXL345Sensor* m_ADXL345;
-//     String m_axeX;
-//     String m_axeY;
-//     String m_axeZ;
-//     uint64_t m_lastReadTime;
-//     uint16_t m_interval;
-//     void getADXL345Values();
-// public:
-//     Program();
-//     void printValues();
-//     void loop();
